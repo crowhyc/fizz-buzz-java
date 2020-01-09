97863e26a248814a7ea3f25b04166938a3136b87
Huangyicai <yicai.huang@thoughtworks.com>
2020-01-09 22:10:38 +0800

    [Add] Achieved fizzBuzz 5 return Buzz.

--- src/main/java/com.thoughtworks/Counter.java
+++ src/main/java/com.thoughtworks/Counter.java
@@ -2,8 +2,13 @@ package com.thoughtworks;
 
 public class Counter {
 
-    public String fizzBuzz(int position) {
-        return "fizz";
-    }
+  public static final String BUZZ = "Buzz";
+  public static final String FIZZ = "Fizz";
 
+  public String fizzBuzz(int position) {
+    if (position % 5 == 0) {
+      return BUZZ;
+    }
+    return FIZZ;
+  }
 }
--- src/test/java/com/thoughtworks/CounterTest.java
+++ src/test/java/com/thoughtworks/CounterTest.java
@@ -3,14 +3,19 @@ package com.thoughtworks;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-import org.hamcrest.core.Is;
 import org.junit.Test;
 
 public class CounterTest {
 
-    Counter counter=new Counter();
-    @Test
-    public void return_fizz_for_3() {
-        assertThat(counter.fizzBuzz(3), is("fizz"));
-    }
+  Counter counter = new Counter();
+
+  @Test
+  public void return_fizz_for_3() {
+    assertThat(counter.fizzBuzz(3), is(Counter.FIZZ));
+  }
+
+  @Test
+  public void return_Buzz_for_5() {
+    assertThat(counter.fizzBuzz(5), is(Counter.BUZZ));
+  }
 }
