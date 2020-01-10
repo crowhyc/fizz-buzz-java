c69cc3c343533950d903e898b766fa67c44d68ae
Huangyicai <yicai.huang@thoughtworks.com>
2020-01-11 00:07:19 +0800

    [Add] when pos num contains 5 ingore role4 5 and num invisibled by 3.

--- src/main/java/com.thoughtworks/SecondCounter.java
+++ src/main/java/com.thoughtworks/SecondCounter.java
@@ -11,27 +11,57 @@ public class SecondCounter {
     public static final String BUZZ = "Buzz";
     public static final String WHIZZ = "WHIZZ";
     public static final String EMPTY_STR = "";
+    private boolean role4Switch;
+    private boolean role5Switch;
+
     private List<CounterRule> ruleList =
             new ArrayList<CounterRule>() {
                 {
-                    add(new CounterRule(pos -> pos.isDivisible(3) ? FIZZ : EMPTY_STR));
-                    add(new CounterRule(pos -> pos.isDivisible(5) ? BUZZ : EMPTY_STR));
-                    add(new CounterRule(pos -> pos.isDivisible(7) ? WHIZZ : EMPTY_STR));
+                    add(
+                            new CounterRule(
+                                    pos -> {
+                                        if (pos.isContains(5)) {
+                                            role5Switch = false;
+                                        }
+                                        return EMPTY_STR;
+                                    }));
+                    add(
+                            new CounterRule(
+                                    pos -> {
+                                        if (pos.isContains(3) && role5Switch) {
+                                            role4Switch = false;
+                                            return FIZZ;
+                                        } else {
+                                            return EMPTY_STR;
+                                        }
+                                    }));
+                    add(
+                            new CounterRule(
+                                    pos ->
+                                            pos.isDivisible(3) && role4Switch && role5Switch
+                                                    ? FIZZ
+                                                    : EMPTY_STR));
+                    add(
+                            new CounterRule(
+                                    pos -> pos.isDivisible(5) && role4Switch ? BUZZ : EMPTY_STR));
+                    add(
+                            new CounterRule(
+                                    pos -> pos.isDivisible(7) && role4Switch ? WHIZZ : EMPTY_STR));
                 }
             };
 
     public String fizzBuzz(Pos pos) {
-        if (pos.isContains(3)) {
-            return FIZZ;
-        }
+        initSwitch();
         String result =
                 ruleList.stream()
                         .map(counterRule -> counterRule.calcRule(pos))
                         .collect(Collectors.joining());
-        if (result.length() == 0) {
-            return pos.getPos();
-        }
-        return result;
+        return result.length() > 0 ? result : pos.getPos();
+    }
+
+    private void initSwitch() {
+        role4Switch = true;
+        role5Switch = true;
     }
 
     class CounterRule {
--- src/test/java/com/thoughtworks/CounterTest2.java
+++ src/test/java/com/thoughtworks/CounterTest2.java
@@ -1,5 +1,8 @@
 package com.thoughtworks;
 
+import static com.thoughtworks.SecondCounter.BUZZ;
+import static com.thoughtworks.SecondCounter.FIZZ;
+import static com.thoughtworks.SecondCounter.WHIZZ;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -10,7 +13,7 @@ public class CounterTest2 {
 
     @Test
     public void return_Fizz_for_3() {
-        assertThat(secondCounter.fizzBuzz(Pos.of(3)), is(SecondCounter.FIZZ));
+        assertThat(secondCounter.fizzBuzz(Pos.of(3)), is(FIZZ));
     }
 
     @Test
@@ -20,23 +23,26 @@ public class CounterTest2 {
 
     @Test
     public void return_Buzz_for_5() {
-        assertThat(secondCounter.fizzBuzz(Pos.of(5)), is(SecondCounter.BUZZ));
+        assertThat(secondCounter.fizzBuzz(Pos.of(5)), is(BUZZ));
     }
 
     @Test
     public void return_Whizz_for_7() {
-        assertThat(secondCounter.fizzBuzz(Pos.of(7)), is(SecondCounter.WHIZZ));
+        assertThat(secondCounter.fizzBuzz(Pos.of(7)), is(WHIZZ));
     }
 
     @Test
-    public void return_FizzBuzzWhizz_for_105() {
-        assertThat(
-                secondCounter.fizzBuzz(Pos.of(105)),
-                is(SecondCounter.FIZZ + SecondCounter.BUZZ + SecondCounter.WHIZZ));
+    public void return_FizzBuzzWhizz_for_210() {
+        assertThat(secondCounter.fizzBuzz(Pos.of(210)), is(FIZZ + BUZZ + WHIZZ));
     }
 
     @Test
     public void return_Fizz_for_13() {
-        assertThat(secondCounter.fizzBuzz(Pos.of(13)), is(SecondCounter.FIZZ));
+        assertThat(secondCounter.fizzBuzz(Pos.of(13)), is(FIZZ));
+    }
+
+    @Test
+    public void return_BuzzWhizz_for_35() {
+        assertThat(secondCounter.fizzBuzz(Pos.of(35)), is(BUZZ + WHIZZ));
     }
 }
